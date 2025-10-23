db44816db432fda4e5f6e375e4d2fed168259c2e
Lennyn M. Chiquezzi <117955252+devLennyn@users.noreply.github.com>
2025-10-23 20:46:07 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Desenvolvimento do projeto para estudo de programação
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Você é meu amor e a Acepipes é uma nersa</h1>
+function Home() {
+  return <h1>Você é meu amor e a Acepipes é uma nersa</h1>;
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
